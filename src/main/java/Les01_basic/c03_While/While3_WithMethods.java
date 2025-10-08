000fe9b02fadd2fb5302d199a8a0032a09a664d4
Denis Ishuk <denis.ishuk@gmail.com>
2025-10-08 15:30:37 +0300

    у

---
+++ src/main/java/Les01_basic/c03_While/While3_WithMethods.java
@@ -0,0 +1,55 @@
+package Les01_basic.c03_While;
+
+import java.util.Scanner;
+
+public class While3_WithMethods {
+    /**
+     * Выдает целое частное от деления
+     *
+     * @param N делимое
+     * @param K делитель
+     * @return целое частное
+     */
+    private static int getQuotient(int N, int K) {
+        int quotient = 0; // частное
+        while (N > K) {
+            N = N - K;
+            quotient++;
+        }
+        return quotient;
+    }
+
+    // для вставки автодока - ВЫШЕ названия метода вставляем: /**
+    /**
+     * Выдает ОСТАТОК от деления нацело
+     * @param N делимое
+     * @param K делитель
+     * @return Остаток
+     */
+    public static int getRemainder(int N, int K) {
+        while (N > K) {
+            N = N - K;   // уменьшается до неделимого
+        }
+        return N;
+    }
+
+    public static void main(String[] args) {
+        /*
+        Даны целые положительные числа N и K.
+        Используя только операции сложения и вычитания,
+        найти частное от деления нацело N на K,
+        а также остаток от этого деления.
+         */
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите N и K, так что N > K: ");
+        int N = in.nextInt(); // делимое
+        int K = in.nextInt(); // делитель
+
+        System.out.printf("Частное = %-3d, Остаток = %-3d%n", getQuotient(N, K), getRemainder(N, K));
+        System.out.println("Программа завершена, нажмите Enter");
+        in.nextLine();
+    }
+}
+
+
---
+++ src/main/java/Les01_basic/c03_While/while3.java
@@ -0,0 +1,31 @@
+package Les01_basic.c03_While;
+
+import java.util.Scanner;
+
+public class while3 {
+    public static void main(String[] args) {
+        /*
+        Даны целые положительные числа N и K.
+        Используя только операции сложения и вычитания,
+        найти частное от деления нацело N на K,
+        а также остаток от этого деления.
+         */
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите N и K, так что N > K: ");
+        int N = in.nextInt(); // делимое
+        int K = in.nextInt(); // делитель
+        int quotient = 0; // частное
+        int remainder = 0; // остаток
+        int Temp = N; // сохраним
+        while (N >= K){
+            N = N - K;  // N -
+            quotient++;
+        }
+        remainder = N;
+        System.out.printf("Частное = %-3d, Остаток = %-3d%n", quotient, N);
+        System.out.println("Программа завершена, нажмите Enter");
+        in.nextLine();
+
+    }
+}
