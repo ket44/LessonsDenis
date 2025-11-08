8ef3c681b33fcc388dfbffed2154af8ce2fd57d0
Ищук Денис <denis.ishuk@gmail.com>
2025-11-08 17:39:52 +0300

    Create task685_GoldPies.java

---
+++ src/main/java/acmp_dn/task685_GoldPies.java
@@ -0,0 +1,58 @@
+package acmp_dn;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class task685_GoldPies {
+
+    public static int getMax(int a, int b, int c) {
+        int max = 0;
+        if (a > b) {
+            if (a > c) {
+                max = a;
+            } else {
+                max = c;
+            }
+        } else {
+            if (b > c) {
+                max = b;
+            } else {
+                max = c;
+            }
+        }
+        return max;
+    }
+
+    public static int getMin(int a, int b, int c) {
+        int min = 0;
+        min = Math.min(Math.min(a, b), c);
+        return min;
+    }
+
+    public static int getMid(int a, int b, int c) {
+        int mid = a + b + c - getMax(a, b, c) - getMin(a, b, c);
+        return mid;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int A1, A2, A3;
+        int B1, B2, B3;
+        A1 = in.nextInt();
+        A2 = in.nextInt();
+        A3 = in.nextInt();
+        B1 = in.nextInt();
+        B2 = in.nextInt();
+        B3 = in.nextInt();
+
+        int result =
+                        getMax(A1, A2, A3) * getMax(B1, B2, B3) +
+                        getMid(A1, A2, A3) * getMid(B1, B2, B3) +
+                        getMin(A1, A2, A3) * getMin(B1, B2, B3);
+
+        System.out.println(result);
+        out.flush();
+    }
+}
