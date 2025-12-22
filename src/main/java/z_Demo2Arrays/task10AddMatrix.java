cdfdefd9e71635825b115616537a1c7fb1a675cc
Ищук Денис <denis.ishuk@gmail.com>
2025-12-23 00:33:38 +0300

    Create task10AddMatrix.java

---
+++ src/main/java/z_Demo2Arrays/task10AddMatrix.java
@@ -0,0 +1,16 @@
+package z_Demo2Arrays;
+
+import java.util.Random;
+
+public class task10AddMatrix {
+    public static void main(String[] args) {
+        /*
+        Демо обработки двумерного массива
+         */
+        final int Rows = 2;
+        final int Columns = 3;
+
+        Random rnd = new Random();
+
+    }
+}
